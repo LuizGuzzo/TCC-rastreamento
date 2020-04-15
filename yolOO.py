bda84fd74b45842bce5c256a6f98c7a467d62346
Luiz <luizguzzo@gmail.com>
2020-04-15 16:21:15 -0300

    YolOO organizado

--- yolOO.py
+++ yolOO.py
@@ -10,17 +10,6 @@ import os
 
 class yoloCNN():
 	
-	# definir parametros iniciais como:
-	# yolo_path, image_path, confidence, threshould
-
-	# COLORS, LABELS
-
-	# image, (H,W) - of image
-
-	# net, layersOutput, idx (NMS)
-
-	# boxes, classesID, confidences
-
 	def __init__(self,yoloPath = "yolo-coco", argConfidence = 0.5, threshold = 0.3):
 
 		self.argConfidence = argConfidence
@@ -41,51 +30,40 @@ class yoloCNN():
 		print("[INFO] loading YOLO from disk...")
 		self.net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-
-##################
-	def get_layersOutputs_image(self,image_path = "videos/maePic.png"):
-
-		# load our input image and grab its spatial dimensions
-		self.image = cv2.imread(image_path)
-		(self.H, self.W) = self.image.shape[:2]
-
 		# determine only the *output* layer names that we need from YOLO
-		ln = self.net.getLayerNames()
-		ln = [ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+		self.ln = self.net.getLayerNames()
+		self.ln = [self.ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
-		# construct a blob from the input image and then perform a forward
-		# pass of the YOLO object detector, giving us our bounding boxes and
-		# associated probabilities
-		blob = cv2.dnn.blobFromImage(self.image, 1 / 255.0, (416, 416),	swapRB=True, crop=False)
+		(self.W, self.H) = (None, None)
+
+
+	def get_objects(self,image):
+
+		# if the frame dimensions are empty, grab them
+		if self.W is None or self.H is None:
+			(self.H, self.W) = image.shape[:2]
+
+
+		blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),	swapRB=True, crop=False)
 		self.net.setInput(blob)
 
-		start = time.time()
-		layerOutputs = self.net.forward(ln)
-		end = time.time()
+		# start = time.time()
+		layerOutputs = self.net.forward(self.ln)
+		# end = time.time()
 
 		# show timing information on YOLO
-		print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+		#print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-		self.layerOutputs = layerOutputs
-
-
-###############################
-
-	def get_objects_from_image(self):
-		
-
-		# initialize our lists of detected bounding boxes, confidences, and
-		# class IDs, respectively
-		self.boxes = []
-		self.confidences = []
-		self.classIDs = []
+		# initialize our lists of detected bounding boxes, confidences, and class IDs, respectively
+		boxes = []
+		confidences = []
+		classIDs = []
 
 		# loop over each of the layer outputs
-		for output in self.layerOutputs:
+		for output in layerOutputs:
 			# loop over each of the detections
 			for detection in output:
-				# extract the class ID and confidence (i.e., probability) of
-				# the current object detection
+				# extract the class ID and confidence (i.e., probability) of the current object detection
 				scores = detection[5:]
 				classID = np.argmax(scores)
 				confidence = scores[classID]
@@ -93,54 +71,46 @@ class yoloCNN():
 				# filter out weak predictions by ensuring the detected
 				# probability is greater than the minimum probability
 				if confidence > self.argConfidence:
-					# scale the bounding box coordinates back relative to the
-					# size of the image, keeping in mind that YOLO actually
-					# returns the center (x, y)-coordinates of the bounding
-					# box followed by the boxes' width and height
+					# scale the bounding box coordinates back relative to the size of the image, keeping in mind that YOLO actually
+					# returns the center (x, y)-coordinates of the bounding box followed by the boxes' width and height
 					box = detection[0:4] * np.array([self.W, self.H, self.W, self.H])
 					(centerX, centerY, width, height) = box.astype("int")
 
-					# use the center (x, y)-coordinates to derive the top and
-					# and left corner of the bounding box
+					# use the center (x, y)-coordinates to derive the top and left corner of the bounding box
 					x = int(centerX - (width / 2))
 					y = int(centerY - (height / 2))
 
-					# update our list of bounding box coordinates, confidences,
-					# and class IDs
-					self.boxes.append([x, y, int(width), int(height)])
-					self.confidences.append(float(confidence))
-					self.classIDs.append(classID)
+					# update our list of bounding box coordinates, confidences, and class IDs
+					boxes.append([x, y, int(width), int(height)])
+					confidences.append(float(confidence))
+					classIDs.append(classID)
 
-		# apply non-maxima suppression to suppress weak, overlapping bounding
-		# boxes
-		self.idxs = cv2.dnn.NMSBoxes(self.boxes, self.confidences, self.argConfidence,
-			self.threshold)
+		# apply non-maxima suppression to suppress weak, overlapping bounding boxes
+		idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.argConfidence, self.threshold)
 
-###############################
 
-	def draw_detected_objects(self):
-		# ensure at least one detection exists
-		if len(self.idxs) > 0:
-			# loop over the indexes we are keeping
-			for i in self.idxs.flatten():
-				# extract the bounding box coordinates
-				(x, y) = (self.boxes[i][0], self.boxes[i][1])
-				(w, h) = (self.boxes[i][2], self.boxes[i][3])
+		# TODO: definir uma estrutura de dados para os objetos identificados, dados: Seu ID, posição, classe
+		# retornar array desta estrutura de dados.
+		# separar a função Draw do metodo de get_objects
+		
+		if len(idxs) > 0:
+			for i in idxs.flatten():
 
-				# draw a bounding box rectangle and label on the image
-				color = [int(c) for c in self.COLORS[self.classIDs[i]]]
-				cv2.rectangle(self.image, (x, y), (x + w, y + h), color, 2)
-				text = "{}: {:.4f}".format(self.LABELS[self.classIDs[i]], self.confidences[i])
-				cv2.putText(self.image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-					0.5, color, 2)
+				(x, y) = (boxes[i][0], boxes[i][1])
+				(w, h) = (boxes[i][2], boxes[i][3])
 
-		# show the output image
-		cv2.imshow("Image", self.image)
-		cv2.waitKey(0)
+				color = [int(c) for c in self.COLORS[classIDs[i]]]
+				cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+				text = "{}: {:.4f}".format(self.LABELS[classIDs[i]],	confidences[i])
+				cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+			# show the output image
+			cv2.imshow("Image", image)
+			cv2.waitKey(0)
 
 
 
-yoloCNN = yoloCNN()
-yoloCNN.get_layersOutputs_image()
-yoloCNN.get_objects_from_image()
-yoloCNN.draw_detected_objects()
+yoloCNN = yoloCNN(yoloPath = "yolo-coco", argConfidence = 0.5, threshold = 0.3)
+image = cv2.imread("videos/maePic.png")
+yoloCNN.get_objects(image)
